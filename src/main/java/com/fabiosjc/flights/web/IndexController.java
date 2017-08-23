c135791623e9e921f2a95ed7718de23f62b7dd26
Fábio Henrique <fabiosjc@gmail.com>
2017-08-23 00:34:21 -0300

    Criação da view em angular + webpack

---
+++ src/main/java/com/fabiosjc/flights/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.fabiosjc.flights.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by fabio on 22/08/17.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/main/webapp/app/app.js
@@ -0,0 +1,27 @@
+import angular from 'angular';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style/app.css';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  constructor() {
+    this.url = 'https://github.com/preboot/angular-webpack';
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
